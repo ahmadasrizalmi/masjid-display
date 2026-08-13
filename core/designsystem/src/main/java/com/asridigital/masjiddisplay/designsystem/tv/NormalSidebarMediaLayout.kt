b00e71930e41727c909687d92a222d5f29cb590f
Ahmad Asri Zalmi <ahmadasrizalmi@gmail.com>
2026-08-13 15:44:29 +0700

    fix(ui): remove invalid Compose weight imports

--- core/designsystem/src/main/java/com/asridigital/masjiddisplay/designsystem/tv/NormalSidebarMediaLayout.kt
+++ core/designsystem/src/main/java/com/asridigital/masjiddisplay/designsystem/tv/NormalSidebarMediaLayout.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
