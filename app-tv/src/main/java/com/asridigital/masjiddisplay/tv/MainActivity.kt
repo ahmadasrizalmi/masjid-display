601caf199e5c5457a9263438ad02796c9d525350
Ahmad Asri Zalmi <ahmadasrizalmi@gmail.com>
2026-08-13 12:35:23 +0700

    feat(tv): render complete NORMAL horizontal media composition

--- app-tv/src/main/java/com/asridigital/masjiddisplay/tv/MainActivity.kt
+++ app-tv/src/main/java/com/asridigital/masjiddisplay/tv/MainActivity.kt
@@ -9,28 +9,37 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.asridigital.masjiddisplay.designsystem.MasjidDisplayColors
-import com.asridigital.masjiddisplay.designsystem.tv.TvHeader
+import com.asridigital.masjiddisplay.designsystem.tv.NormalHorizontalMediaLayout
+import com.asridigital.masjiddisplay.designsystem.tv.PrayerBarItem
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent { TvHeaderDevelopmentScreen() }
+        setContent { NormalHorizontalMediaDevelopmentScreen() }
     }
 }
 
 @Composable
-private fun TvHeaderDevelopmentScreen() {
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(MasjidDisplayColors.Background),
-    ) {
-        TvHeader(
-            currentTime = "13:48",
-            mosqueName = "MASJID NURUL HIKMAH",
-            location = "Sleman, Daerah Istimewa Yogyakarta",
-            gregorianDate = "Kamis, 13 Agustus 2026",
-            hijriDate = "29 Safar 1448 H",
+private fun NormalHorizontalMediaDevelopmentScreen() {
+    NormalHorizontalMediaLayout(
+        currentTime = "13:48",
+        mosqueName = "MASJID NURUL HIKMAH",
+        location = "Sleman, Daerah Istimewa Yogyakarta",
+        gregorianDate = "Kamis, 13 Agustus 2026",
+        hijriDate = "29 Safar 1448 H",
+        prayers = listOf(
+            PrayerBarItem("SUBUH", "04:32"),
+            PrayerBarItem("SYURUQ", "05:47"),
+            PrayerBarItem("DZUHUR", "11:46", isHighlighted = true, countdown = "dalam 01:18"),
+            PrayerBarItem("ASHAR", "15:07"),
+            PrayerBarItem("MAGHRIB", "17:42"),
+            PrayerBarItem("ISYA", "18:54"),
+        ),
+    ) { _ ->
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(MasjidDisplayColors.SurfaceMuted),
         )
     }
 }
