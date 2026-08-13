7e31778dbf6a3f4b9d1dd17710540f43ccf64493
Ahmad Asri Zalmi <ahmadasrizalmi@gmail.com>
2026-08-13 08:14:43 +0700

    feat(ui): add initial TV design tokens

---
+++ core/designsystem/src/main/java/com/asridigital/masjiddisplay/designsystem/MasjidDisplayTokens.kt
@@ -0,0 +1,30 @@
+package com.asridigital.masjiddisplay.designsystem
+
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+object MasjidDisplayColors {
+    val Background = Color(0xFFF6F7F5)
+    val Surface = Color(0xFFFFFFFF)
+    val TextPrimary = Color(0xFF17201B)
+    val TextSecondary = Color(0xFF68736D)
+    val Divider = Color(0xFFDDE2DE)
+    val Accent = Color(0xFF176B45)
+    val OnAccent = Color(0xFFFFFFFF)
+    val Warning = Color(0xFFD99518)
+}
+
+object TvDimensions {
+    val HeaderHeight = 170.dp
+    val HeaderHorizontalPadding = 64.dp
+    val HeaderVerticalPadding = 24.dp
+}
+
+object TvTypography {
+    val Clock = 104.sp
+    val MosqueName = 44.sp
+    val Location = 26.sp
+    val DatePrimary = 30.sp
+    val DateSecondary = 25.sp
+}
